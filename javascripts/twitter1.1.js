ab1a67b24ee4884f192735ea1eab5ed4fee04fb5
Sarim Khan <sarim2005@gmail.com>
2013-11-06 02:30:13 +0600

    Site updated at 2013-11-05 20:30:13 UTC

--- javascripts/twitter1.1.js
+++ javascripts/twitter1.1.js
@@ -1,18 +1,18 @@
-function parsetwitter(tweetcount){
+function parsetwitter(){
     frms = document.getElementsByTagName("iframe");
     for (i=0;i<frms.length;++i){
-        if (frms[i].id.search("twitter-widget-") != -1){
-            tweets = frames[frms[i].id].document.body.getElementsByClassName("e-entry-content");
+        if (frms[i].id.search("twitter-widget-") !== -1){
+            tweets = frms[i].contentWindow.document.body.getElementsByClassName("e-entry-content");
             if (tweets.length == 0) return setTimeout(parsetwitter,500);
             
-            tweetmeta = frames[frms[i].id].document.body.getElementsByClassName("permalink");
+            tweetmeta = frms[i].contentWindow.document.body.getElementsByClassName("permalink");
             content = "";
             
             for (p=0;p<tweets.length;++p){
-                txt = tweets[p].innerText;
+                txt = tweets[p].textContent;
                 link = tweetmeta[p].href;
-                ttime = tweetmeta[p].innerText;
-                content += '<li>'+'<p>'+'<a href="' + link + '">'+ ttime +'</a>'+ linkifyTweet(txt.replace(/\n/g, '<br>')) +'</p>'+'</li>';
+                ttime = tweetmeta[p].textContent;
+                content += '<li>'+'<p>'+'<a class="tweetdate" href="' + link + '">'+ ttime +'</a>'+ linkifyTweet(txt.replace(/\n/g, '<br>')) +'</p>'+'</li>';
                 
             }
             
@@ -23,11 +23,9 @@ function parsetwitter(tweetcount){
 
 
 function linkifyTweet(text) {
-    console.log(text);
-  // Linkify urls, usernames, hashtags
+  // Linkify urls, usernames
   text = text.replace(/(https?:\/\/)([\w\-:;?&=+.%#\/]+)/gi, '<a href="$1$2">$2</a>')
     .replace(/(^|\W)@(\w+)/g, '$1<a href="http://twitter.com/$2">@$2</a>');
   
-  console.log(text);
   return text;
 }
